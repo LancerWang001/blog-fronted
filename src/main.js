eaf28a71d8472e0cd235a030c243c731e3812b9d
LancerWang001 <wangandong001@gmail.com>
2022-03-31 08:46:56 +0800

    init: init project

---
+++ src/main.js
@@ -0,0 +1,21 @@
+// This is the main.js file. Import global CSS and scripts here.
+// The Client API can be used here. Learn more: gridsome.org/docs/client-api
+import DefaultLayout from '~/layouts/Default.vue'
+import dayjs from 'dayjs'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import './assets/css/index.css'
+
+export default function (Vue, { router, head, isClient }) {
+  // Set default layout as a global component
+  Vue.component('Layout', DefaultLayout)
+  Vue.filter('date', (dateStr, format = 'MMM DD, YYYY') => dayjs(dateStr).format(format))
+  Vue.mixin({
+    data () {
+      return {
+        GRIDSOME_API_URL: process.env.GRIDSOME_API_URL
+      }
+    }
+  })
+}
